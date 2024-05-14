e12a4a5ad6e0d9c3475bf736475fcee5dd977fb1
KhaiDucDuong <greenjohnny135@gmail.com>
2024-05-15 05:51:23 +0700

    Delete unused repositories

--- src/main/java/com/example/onlinebankingapp/repositories/InstructorRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.onlinebankingapp.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorRepository extends JpaRepository<InstructorEntity, Long>{
-}
--- src/main/java/com/example/onlinebankingapp/repositories/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.onlinebankingapp.repositories;
-import com.example.onlinebankingapp.entities.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<UserEntity, Long>{
-}
