fb53d4397cf76be558c408625295e5f10768dc4a
KhaiDucDuong <greenjohnny135@gmail.com>
2024-05-15 23:15:56 +0700

    Create AdminEntity.java

---
+++ src/main/java/com/example/onlinebankingapp/entities/AdminEntity.java
@@ -0,0 +1,22 @@
+package com.example.onlinebankingapp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name ="admins")
+public class AdminEntity {
+    @Id
+    @Column(name="adminId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+}
