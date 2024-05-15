2d6215aef314e7831735191a70aaf79a33634d85
KhaiDucDuong <greenjohnny135@gmail.com>
2024-05-15 23:46:45 +0700

    comment Employee code

--- src/main/java/com/example/onlinebankingapp/repositories/EmployeeRepository.java
+++ src/main/java/com/example/onlinebankingapp/repositories/EmployeeRepository.java
@@ -1,15 +1,15 @@
-package com.example.onlinebankingapp.repositories;
-
-import com.example.onlinebankingapp.entities.EmployeeEntity;
-import com.example.onlinebankingapp.entities.enums.EmployeeRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
-    List<EmployeeEntity> getAllByRole(EmployeeRole employeeRole);
-
-    EmployeeEntity getEmployeeEntitiesByCitizenId(String citizenId);
-}
+//package com.example.onlinebankingapp.repositories;
+//
+//import com.example.onlinebankingapp.entities.EmployeeEntity;
+//import com.example.onlinebankingapp.entities.enums.EmployeeRole;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+//    List<EmployeeEntity> getAllByRole(EmployeeRole employeeRole);
+//
+//    EmployeeEntity getEmployeeEntitiesByCitizenId(String citizenId);
+//}
